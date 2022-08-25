0b93cc77500a481cc2608423126e16c97a6781a4
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-24 20:24:59 -0600

    break rspec tests up into correct seperate files

---
+++ spec/features/park/index_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "park index page", type: :feature do
+  describe "as a visitor" do
+
+    describe 'when I visit /parks' do
+
+      it 'I see each Park name in the system' do 
+        park_1 = Park.create!(name: 'Nancy Lewis Park', acres: 8.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
+        park_2 = Park.create!(name: 'America the Beautiful Park', acres: 16.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
+        park_3 = Park.create!(name: 'Garden of the Gods', acres: 1341.3, visitor_center: true, playground: false, opening_hour: 5, closing_hour: 9)
+
+        visit '/parks'
+        save_and_open_page
+
+        expect(page).to have_content(park_1.name)
+        expect(page).to have_content(park_2.name)
+        expect(page).to have_content(park_3.name)
+      end
+      
+    end
+  end
+  # xit 'I see each child in the system' do
+  #   city = City.create!(name: 'Colorado Springs', population: 100, state_capital: false)
+
+  #   park_1 = Park.create!(name: 'Nancy Lewis Park', acres: 8.9)
+  # end
+  # it "including the child's attributes"
+end
