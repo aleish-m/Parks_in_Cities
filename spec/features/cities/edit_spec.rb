0c2690ba6d4141946f0ba49be55a25c8549f36d2
Aleisha Mork <aleisha.mork@gmail.com>
2022-08-28 00:09:43 -0600

    add a link next to each city on the city index page to update that cities info

--- spec/features/cities/edit_spec.rb
+++ spec/features/cities/edit_spec.rb
@@ -4,25 +4,41 @@ RSpec.describe 'cities edit page', type: :feature do
 
   describe "as a visitor" do
 
-    describe 'when I visit /cities/:id' do
-      before :each do
-
-        @city = City.create!(name: 'Colorado Springs', population: 100, state_capital: false)
-
-        @park_1 = @city.parks.create!(name: 'Nancy Lewis Park', acres: 8.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
-        @park_2 = @city.parks.create!(name: 'America the Beautiful Park', acres: 16.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
-        @park_3 = @city.parks.create!(name: 'Garden of the Gods', acres: 1341.3, visitor_center: true, playground: false, opening_hour: 5, closing_hour: 9)
-
-      end
-
-      it 'I see a link to "Update City", when I click the link "Update City" then I am taken to "/cities/:id/edit"' do
-        visit "/cities/#{@city.id}"
+    before :each do
+      
+      @city = City.create!(name: 'Colorado Springs', population: 100, state_capital: false)
+      
+      @park_1 = @city.parks.create!(name: 'Nancy Lewis Park', acres: 8.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
+      @park_2 = @city.parks.create!(name: 'America the Beautiful Park', acres: 16.9, visitor_center: false, playground: true, opening_hour: 5, closing_hour: 10)
+      @park_3 = @city.parks.create!(name: 'Garden of the Gods', acres: 1341.3, visitor_center: true, playground: false, opening_hour: 5, closing_hour: 9)
+    end
+  
+  
+    describe 'when I visit /cities' do
+      
+      it 'Next to every city, I see a link to edit that citys info hen I click the link I should be taken to that city edit page where I can update its information'do
+        visit '/cities'
         save_and_open_page
-
+        
         find_link('Update City Info').visible?
         click_link 'Update City Info'
         expect(page).to have_current_path("/cities/#{@city.id}/edit")
       end
+    end
+
+    describe 'when I visit /cities/:id' do
+
+      it 'I see a link to "Update City", when I click the link "Update City" then I am taken to "/cities/:id/edit"' do
+        visit "/cities/#{@city.id}"
+        save_and_open_page
+        
+        find_link('Update City Info').visible?
+        click_link 'Update City Info'
+        expect(page).to have_current_path("/cities/#{@city.id}/edit")
+      end
+    end
+    
+    describe 'when I visit /cities/:id/edit' do
 
       it 'When I am on "/cities/:id/edit" I  see a form to edit the city attributes:' do
         visit "/cities/#{@city.id}/edit"
